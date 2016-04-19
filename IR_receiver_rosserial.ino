fe0342d0ef8c82ccf569418ba33710d8167b27ef
wennycooper <kevin.kuei.0321@gmail.com>
2016-04-19 09:29:48 +0800

    accumulate readings

--- IR_receiver_rosserial.ino
+++ IR_receiver_rosserial.ino
@@ -2,13 +2,19 @@
 #include <std_msgs/UInt16.h>
 #include <geometry_msgs/Vector3.h>
 
-#define LOOPTIME        100
+#define LOOPTIME1         10
+unsigned long lastMilli1 = 0;
+
+#define LOOPTIME2        100
+unsigned long lastMilli2 = 0;
+
+
 
 ros::NodeHandle nh;
 //std_msgs::UInt16 irValue;
 geometry_msgs::Vector3 irValue;
 ros::Publisher pub("irValue", &irValue);
-unsigned long lastMilli = 0;
+
 
 void setup() {
   nh.initNode();
@@ -18,12 +24,23 @@ void setup() {
 }
 
 void loop() {
-  if ((millis()-lastMilli) >= LOOPTIME)   {        // enter timed loop
-    lastMilli = millis();
-    irValue.x = analogRead(A0);
-    irValue.y = analogRead(A1);
-    pub.publish(&irValue);
+  if ((millis()-lastMilli1) >= LOOPTIME1)   {  // time to read ADC
+    lastMilli1 = millis();
 
+    // accumulate those readings
+    irValue.x += analogRead(A0);
+    irValue.y += analogRead(A1);
+    //pub.publish(&irValue);
+
+  }
+  
+  if ((millis()-lastMilli2) >= LOOPTIME2)   {  // time to pub
+    lastMilli2 = millis();
+    //irValue.x = analogRead(A0);
+    //irValue.y = analogRead(A1);
+    pub.publish(&irValue);
+    irValue.x = 0;
+    irValue.y = 0;
   }
     nh.spinOnce();
 
