f6431df48c18a396b9d6aa3c891ef871247f77bf
wennycooper <kevin.kuei.0321@gmail.com>
2016-04-26 18:19:20 +0800

    add loop for 100 times

--- IR_receiver_rosserial.ino
+++ IR_receiver_rosserial.ino
@@ -5,7 +5,7 @@
 #define LOOPTIME1         10
 unsigned long lastMilli1 = 0;
 
-#define LOOPTIME2        100
+#define LOOPTIME2        1000
 unsigned long lastMilli2 = 0;
 
 
@@ -22,8 +22,25 @@ void setup() {
   Serial.begin(57600);
   
 }
-
+ unsigned int i=0;
+unsigned lastTime = 0;
 void loop() {
+  //lastTime = millis();
+  for (i=0; i<100; i++) {
+    irValue.x += analogRead(A0);
+    irValue.y += analogRead(A1);  
+  }
+//  Serial.println(millis() - lastTime);
+
+  irValue.x = irValue.x/100;
+  irValue.y = irValue.y/100;
+
+// publish and clean up //  
+  pub.publish(&irValue);
+  irValue.x = 0;
+  irValue.y = 0;  
+  
+/*
   if ((millis()-lastMilli1) >= LOOPTIME1)   {  // time to read ADC
     lastMilli1 = millis();
 
@@ -33,6 +50,7 @@ void loop() {
     //pub.publish(&irValue);
 
   }
+
   
   if ((millis()-lastMilli2) >= LOOPTIME2)   {  // time to pub
     lastMilli2 = millis();
@@ -42,6 +60,9 @@ void loop() {
     irValue.x = 0;
     irValue.y = 0;
   }
+
+*/
     nh.spinOnce();
 
 }
+
